84d27f26cd629dadd51a68c6b4b60e7e21c682c8
Corban Swain <CorbanSwain@gmail.com>
2018-07-15 12:11:47 -0400

    adding more fields

--- directory_parse.py
+++ directory_parse.py
@@ -6,13 +6,16 @@ import time
 in_filename = 'bgsa_mailman_roster.txt'
 with open(in_filename, 'r') as fle:
     search_terms = [l.strip() for l in fle.readlines()]
-search_terms = ['ifueko', 'c_swain']
-re_program = re.compile(r'(?<=<PRE>\s{6}name:\s)'
-                        '(?P<last_name>.*),\s+'
-                        '(?P<first_name>\S*?)'
-                        '(\s+(?P<middle_name>\S*))?'
-                        '(?=\\n)')
-match_group_keys = ['first_name', 'middle_name', 'last_name']
+re_program = re.compile(r'<PRE>\s+name: '
+                        '(?P<last_name>.*),\s+(?P<first_name>\S*?)'
+                        '(\s+(?P<middle_name>\S*?)\.?)?\\n(.*\\n)*?'
+                        '(\s*email:.*?(?P<email>\w+@\w+?\.\w+).*\\n)?(.*\\n)*?'
+                        '(\s*department: (?P<department>.*?)\\n)?(.*\\n)*?'
+                        '(\s*school: (?P<school>.*?)\s*\\n)?(.*\\n)*?'
+                        '(\s*year: (?P<year>.*?)\\n)?(.*\\n)*?'
+                        '(\s*title: (?P<title>.*?)\\n)?')
+match_group_keys = ['first_name', 'middle_name', 'last_name', 'email',
+                    'department', 'school', 'year', 'title']
 address_fmt = 'http://web.mit.edu/bin/cgicso?options=general&query=%s'
 
 results = {}
@@ -23,11 +26,11 @@ for _ in range(repeats):
         tries = 0
         while True:
             tries += 1
-            time.sleep(0.1)
             r = requests.get(address_fmt % term)
-            if r.status_code == 200:
+            code = r.status_code
+            if code == 200:
                 break
-            elif r.status_code == 500:
+            elif code == 500:
                 pause_time = 1
                 pp.pprint('Too many requests, taking a %3.1f minute pause.'
                           % pause_time)
@@ -35,16 +38,17 @@ for _ in range(repeats):
             else:
                 if tries > 5:
                     pp.pprint(('Unknown Status Code: %d ... Search: \'%s\''
-                               ' ... continuing.')
-                              % (r.status_code, term))
+                               ' ... continuing to next search.')
+                              % (code, term))
                     break
                 else:
                     pp.pprint(('Unknown Status Code: %d ... Search: \'%s\''
                                ' ... trying again.')
-                              % (r.status_code, term))
+                              % (code, term))
+                    time.sleep(1)
         m = re_program.search(r.text)
         pp.pprint('%3d | %3d (/ %3d) --- EC: %3d --- \'%s\''
-                  % (i, match_count, len(search_terms), r.status_code, term))
+                  % (i, match_count, len(search_terms), code, term))
         results[term] = [m.group(k) if m
                          and k in m.groupdict()
                          and m.group(k)
@@ -57,7 +61,7 @@ if results:
     match_group_keys.insert(0, 'search_term')
     results.insert(0, match_group_keys)
     pp.pprint(results)
-    with open('output.csv', 'w+') as fle:
-        [fle.write(', '.join(l) + '\n') for l in results]
+    with open('output.tsv', 'w+') as fle:
+        [fle.write('\t'.join(l) + '\n') for l in results]
 
 
